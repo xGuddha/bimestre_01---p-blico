c8615773b0d005b5e49c48e32eaf8f977750b35b
gvrDev <guilhermevrodrigues123@gmail.com>
2022-08-24 19:13:44 -0300

    atividade_02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Guilherme Vinicius Rodrigues
-        </p>
+        </h1>
+        <Evento data="2 de agosto de 2019 Sexta-Feira" horario="18h30 - 00h00" local="Parque do Povo" cidade="Campina Grande/PR" genero="Arte em forma de SERTANEJO" />
+        <ul>
+          <Ingressos nome="Camarote" valor="R$100,00" />
+          <Ingressos nome="Camarote Open Bar" valor="R$150,00" />
+          <Ingressos nome="Front-stage" valor="R$60,00" />
+          <Ingressos nome="Pista" valor="R$30,00" />
+        </ul>
       </header>
     </div>
   );
+}
+
+class Evento extends React.Component {
+  render() {
+    return (
+      <p>
+        Data: {this.props.data} <br />
+        Horario: {this.props.horario} <br />
+        Local: {this.props.local} <br />
+        Cidade: {this.props.cidade} <br />
+        Genero: {this.props.genero} <br />
+      </p>);
+  }
+}
+
+class Ingressos extends React.Component {
+  render() {
+    return (
+      <li>{this.props.nome}: {this.props.valor}</li>
+    );
+  }
 }
 
 export default App;
