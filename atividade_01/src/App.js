c011f0c8ab2f993d55930ea7b050b31a10464ce3
gvrDev <guilhermevrodrigues123@gmail.com>
2022-09-06 20:18:28 -0300

    reestruturacao projeto

--- atividade_01/src/App.js
+++ atividade_01/src/App.js
@@ -6,10 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Gustavo Lima
-        </h1>
         <Evento data="2 de agosto de 2019 Sexta-Feira" horario="18h30 - 00h00" local="Parque do Povo" cidade="Campina Grande/PR" genero="Arte em forma de SERTANEJO" />
+        <h1> Ingressos </h1>
         <ul style={{ textAlign: "left" }}>
           <Ingressos nome="Camarote" valor="R$100,00" />
           <Ingressos nome="Camarote Open Bar" valor="R$150,00" />
@@ -24,13 +22,16 @@ function App() {
 class Evento extends React.Component {
   render() {
     return (
-      <p style={{ textAlign: "left" }}>
-        <b>Data:</b> {this.props.data} <br />
-        <b>Horario:</b> {this.props.horario} <br />
-        <b>Local:</b> {this.props.local} <br />
-        <b>Cidade:</b> {this.props.cidade} <br />
-        <b>Genero:</b> {this.props.genero} <br />
-      </p>);
+      <div>
+        <h1>Gustavo Lima</h1>
+        <p style={{ textAlign: "left" }}>
+          <b>Data:</b> {this.props.data} <br />
+          <b>Horario:</b> {this.props.horario} <br />
+          <b>Local:</b> {this.props.local} <br />
+          <b>Cidade:</b> {this.props.cidade} <br />
+          <b>Genero:</b> {this.props.genero} <br />
+        </p>
+      </div>);
   }
 }
 
